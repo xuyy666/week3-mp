2461a753022369a715c1d7603949bda974f6d9e4
xuyy121840 <xuyy163.com>
2018-12-17 10:10:29 +0800

    详情页排版

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,16 @@ init();
 
 function init() {
     addEvent();
+    canshu();
 }
 
 function addEvent() {
     $('.search-btn').on('click', function() {
-        location.href = '../page/detail.html';
+        location.href = '../page/detail.html?date=' + date;
     })
+};
+
+
+function canshu() {
+
 }
